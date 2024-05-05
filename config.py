4e1de819f357f13eba888e9ba576920fd78f2727
NAIAlashova <an.lashova@yandex.ru>
2024-05-05 21:13:23 +0400

    first commit

---
+++ config.py
@@ -0,0 +1,20 @@
+HOME_DIR = 'C:/pythonProject/FinalProject'
+
+LOGS = f'{HOME_DIR}/log.txt'
+DB_FILE = f'{HOME_DIR}/messages.db'
+
+IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'
+FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'
+BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'
+
+query1 = '''CREATE TABLE IF NOT EXISTS 'Requests' (
+id INTEGER PRIMARY KEY, 
+user_id INTEGER, 
+role TEXT,
+contents TEXT,
+tokens INTEGER,
+blocks INTEGER);'''
+
+hi = 'Привет. Я - бот и твой помощник.'
+help = 'Что случилось? Если я тебя не понимаю, сорри. Такое бывает, просто спроси по-другому. Но может быть, это я сломался. Тогда тебе придётся подождать, пока меня не исправят.'
+about = 'Я - бот-помощник. Меня подключили к нейросети, и теперь я могу общаться с тобой как человек. Даже присылать голосовые.'
