a75a5f939839993376288e5744d2288db5dbbd2d
NAIAlashova <an.lashova@yandex.ru>
2024-05-09 20:53:49 +0400

    dop commit

--- creds.py
+++ creds.py
@@ -5,24 +5,19 @@ from datetime import datetime
 import requests
 from config import LOGS, IAM_TOKEN_PATH, FOLDER_ID_PATH, BOT_TOKEN_PATH
 
-logging.basicConfig(filename=LOGS, level=logging.INFO, format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s", filemode="w")
-
+logging.basicConfig(filename=log, level=logging.INFO, format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s", filemode="w")
 def create_new_token():
-    url = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
+    metadata_url = "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token"
     headers = {"Metadata-Flavor": "Google"}
     try:
-        response = requests.get(url=url, headers=headers)
+        response = requests.get(metadata_url, headers=headers)
         if response.status_code == 200:
-            token_data = response.json()
-            token_data['expires_at'] = time.time() + token_data['expires_in']
-            with open(IAM_TOKEN_PATH, "w") as token_file:
-                json.dump(token_data, token_file)
             logging.info("Получен новый iam_token")
+            return response.json()
         else:
             logging.error(f"Ошибка получения iam_token. Статус-код: {response.status_code}")
     except Exception as e:
         logging.error(f"Ошибка получения iam_token: {e}")
-
 def get_creds():
     try:
         with open(IAM_TOKEN_PATH, 'r') as f:
@@ -39,7 +34,6 @@ def get_creds():
     with open(FOLDER_ID_PATH, 'r') as f:
         folder_id = f.read().strip()
     return iam_token, folder_id
-
 def get_bot_token():
     with open(BOT_TOKEN_PATH, 'r') as f:
         return f.read().strip()
